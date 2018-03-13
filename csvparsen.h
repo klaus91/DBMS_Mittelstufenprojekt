2f4479e4fc545ce0e03cee1b6eef4fea1f6870d5
jared2s <32058920+jared2s@users.noreply.github.com>
2018-03-13 17:21:40 +0100

    Added unimplemented methods to datenverwaltung.cpp Declaired showTable in mainwindow.h

--- csvparsen.h
+++ csvparsen.h
@@ -14,7 +14,6 @@ public:
     virtual bool oeffneDatei(QString dateiPfad);
     virtual void leseDatei(QFile & file);
     virtual void schreibeDatei();
-//    void CsvParsen::leseDatei();
 
 
 };
--- datenverwaltung.cpp
+++ datenverwaltung.cpp
@@ -5,3 +5,37 @@ Datenverwaltung::Datenverwaltung(QWidget *parent) : QMainWindow(parent)
 
 }
 
+void eintragErstellen()
+{
+
+}
+
+void eintragSuchen()
+{
+
+}
+
+void eintragEntfernen()
+{
+
+}
+
+void eintragSortieren()
+{
+
+}
+
+void tabelleAnlegen()
+{
+
+}
+
+void eintragEditieren()
+{
+
+}
+
+void fehlerHandhabung()
+{
+
+}
